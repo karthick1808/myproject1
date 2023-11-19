27e7b1ae4229f373b9e86f310d17b4d7624829ea
Karthick1808 <karthickspikey@gmail.com>
2023-11-19 14:17:48 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myproject1',
+    version='1.0.0',
+    packages=find_packages(),
+    url='https://github.com/karthick1808/myproject1',
+    author_email="karthick180887@gmail.com",
+    author='Karthick Selvam',
+    install_requires=[
+        # Add your project dependencies here
+    ],
+)
---
+++ test/test_main.py
@@ -0,0 +1,4 @@
+from mymodule.main import add_numbers
+
+def test_add_numbers():
+    assert add_numbers(2, 3) == 5
