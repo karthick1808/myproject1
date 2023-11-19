78bae6c93738d0f3ddac9aa47600414ea22e30d7
Karthick1808 <karthickspikey@gmail.com>
2023-11-19 16:07:25 +0530

    Update build.py

--- build.py
+++ build.py
@@ -3,7 +3,7 @@
 from pybuilder.core import use_plugin, init, task
 
 use_plugin("python.core", version="0.13.10")
-use_plugin("python.unittest")
+use_plugin("python.unittest", {"output_directory": "target/reports/tests"})
 use_plugin("python.coverage")
 use_plugin("python.distutils")
 use_plugin("python.pycharm")
