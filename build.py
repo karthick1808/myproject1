8522a3656d3ed3da9d9ea2918a05e495c72cb068
Karthick1808 <karthickspikey@gmail.com>
2023-11-19 15:17:26 +0530

    Update build.py

--- build.py
+++ build.py
@@ -2,7 +2,7 @@
 
 from pybuilder.core import use_plugin, init
 
-use_plugin("python.core")
+use_plugin("python.core", version="0.13.10")
 use_plugin("python.unittest")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
