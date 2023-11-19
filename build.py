5a2fbb59964ae930d9f2d978dc0dca671c2428c4
Karthick1808 <karthickspikey@gmail.com>
2023-11-19 15:42:52 +0530

    Update build.py

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 # build.py
 
-from pybuilder.core import use_plugin, init
+from pybuilder.core import use_plugin, init, task
 
 use_plugin("python.core", version="0.13.10")
 use_plugin("python.unittest")
@@ -21,3 +21,18 @@ default_task = "publish"
 def set_properties(project):
     project.depends_on_requirements("requirements.txt")
     project.build_depends_on("pytest")
+
+@task
+def publish(project, logger):
+    # Add your publishing logic here
+    logger.info("Publishing the package")
+
+
+@task("analyze")
+def analyze():
+    pass  # Add analysis tasks as needed
+
+
+@task("clean")
+def clean():
+    pass  # Add clean-up tasks as needed
